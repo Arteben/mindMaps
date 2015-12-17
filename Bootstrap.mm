9adbce29b1c18693afa99bba7849307c7b47f584
Artjom <artjombebenin@gmail.com>
2015-12-17 21:18:14 +0300

    create bootstrap.mm

---
+++ Bootstrap.mm
@@ -0,0 +1,95 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1450367719969" BACKGROUND_COLOR="#6600ff"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ffffff" size="5"><b>&#160;&#160;&#160;_ _ Bootstrap _ _</b></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<hook NAME="MapStyle" background="#f0f2f0" zoom="1.467">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000099" STYLE="fork">
+<font NAME="Verdana" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#000001" BACKGROUND_COLOR="#ffeb89" STYLE="bubble" FORMAT="STANDARD_FORMAT" MIN_WIDTH="1">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f7f7f7" STYLE="bubble" FORMAT="NO_FORMAT" MAX_WIDTH="2000">
+<font NAME="Monospaced" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ff0000">
+<icon BUILTIN="yes"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<cloud COLOR="#ffffff" SHAPE="ARC"/>
+</stylenode>
+<stylenode TEXT="&#x41f;&#x435;&#x440;&#x435;&#x447;&#x438;&#x441;&#x43b;&#x435;&#x43d;&#x438;&#x435;" STYLE="fork" FORMAT="STANDARD_FORMAT" MIN_WIDTH="1">
+<font NAME="SansSerif" BOLD="false" ITALIC="false"/>
+<edge STYLE="horizontal"/>
+</stylenode>
+<stylenode TEXT="&#x441;&#x43f;&#x438;&#x441;&#x43e;&#x43a;&#x411;&#x435;&#x437;&#x420;&#x430;&#x43c;&#x43a;&#x438;" COLOR="#000000" BACKGROUND_COLOR="#e8e8e8" STYLE="bubble">
+<font NAME="Verdana" SIZE="10" BOLD="false"/>
+<edge STYLE="horizontal"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font NAME="URW Gothic L" SIZE="18" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="uses" POSITION="right" ID="ID_1663049870" CREATED="1450370843665" MODIFIED="1450370850426">
+<edge COLOR="#00ff00"/>
+<node TEXT="        &lt;!DOCTYPE html&gt;&#xa;        &lt;html&gt;&#xa;          &lt;head&gt;&#xa;            &lt;title&gt;&#x428;&#x430;&#x431;&#x43b;&#x43e;&#x43d; Bootstrap 101&lt;/title&gt;&#xa;            &lt;!-- Bootstrap --&gt;&#xa;            &lt;link href=&quot;css/bootstrap.min.css&quot; rel=&quot;stylesheet&quot;&gt;&#xa;          &lt;/head&gt;&#xa;          &lt;body&gt;&#xa;            &lt;h1&gt;&#x41f;&#x440;&#x438;&#x432;&#x435;&#x442;, &#x43c;&#x438;&#x440;!&lt;/h1&gt;&#xa;            &lt;script src=&quot;http://code.jquery.com/jquery-latest.js&quot;&gt;&lt;/script&gt;&#xa;            &lt;script src=&quot;js/bootstrap.min.js&quot;&gt;&lt;/script&gt;&#xa;          &lt;/body&gt;&#xa;        &lt;/html&gt;" LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_1167586506" CREATED="1450370889757" MODIFIED="1450370894572"/>
+</node>
+<node TEXT="" POSITION="right" ID="ID_445337728" CREATED="1450367089275" MODIFIED="1450367269850">
+<icon BUILTIN="help"/>
+<edge COLOR="#ff0000"/>
+<node TEXT="&#x442;&#x440;&#x435;&#x431;&#x443;&#x435;&#x442; jquery.js" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_993897637" CREATED="1450370813931" MODIFIED="1450370830311"/>
+<node TEXT="&#x444;&#x440;&#x43e;&#x439;&#x43c;&#x43e;&#x432;&#x438;&#x43a; &#x434;&#x43b;&#x44f; &#x431;&#x44b;&#x441;&#x442;&#x440;&#x43e;&#x439; &#x440;&#x430;&#x437;&#x440;&#x43e;&#x431;&#x43e;&#x442;&#x43a;&#x438; front-end" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1747705343" CREATED="1450367280086" MODIFIED="1450367316094"/>
+</node>
+</node>
+</map>
